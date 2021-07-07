bcf9d85d6eb33115fbdda2b58c98384b8e6a8382
eunjeong0417 <82692118+eunjeong0417@users.noreply.github.com>
2021-07-07 23:40:00 +0900

    commit

--- index.js
+++ index.js
@@ -11,3 +11,29 @@
 // //실제적으로 mouse의 좌표를 확인하려면 mousemove
 // //mousemove 이벤트의 pageX, pageY좌표를 확인한다
 
+//상수 변수 const로 만든다
+const lineyEl = document.getElementById('line-Y')
+const linexEl = document.getElementById('line-X')
+const imgEl = document.querySelector('.target')
+const spanEl = document.getElementById('tag')
+
+window.addEventListener('mousemove', (e)=>{
+    //동일하게 사용하는 값은 const 변수로 만들어 사용한다
+    const X = e.pageX;
+    const Y = e.pageY;
+
+    //스타일을 지정할때는 px를 붙여준다
+    //pageX, pageY 좌표를 style 속성으로 넣어준다
+    //style.top은 Y축
+    //style.left는 X축
+    imgEl.style.top = `${Y}px`
+    imgEl.style.left = `${X}px`
+//Template literals 사용
+    lineyEl.style.left = `${X}px`
+    linexEl.style.top = `${Y}px`
+
+    spanEl.style.top = `${Y}px`
+    spanEl.style.left = `${X}px`
+//style.left 값으로 pageX 좌표를 넣는다
+    spanEl.textContent = `pageX : ${X}px, pageY : ${Y}px`
+})
