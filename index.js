8723022bc36c04498f174244d1ccd406ddbf148a
eunjeong0417 <82692118+eunjeong0417@users.noreply.github.com>
2021-07-07 22:21:38 +0900

    2 commit

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-const cursorEl = document.getElementById('cursor')
+// const cursorEl = document.getElementById('cursor')
+// const imgEl = document.getElementsByTagName('img')
 
-window.addEventListener('mousemove', (e) => {
-    cursorEl.style.top =`${e.pageY}px`
-    cursorEl.style.left = `${e.pageX}px`;
-})
-//window에 mousemove 이벤트 추가
-//실제적으로 mouse의 좌표를 확인하려면 mousemove
-//mousemove 이벤트의 pageX, pageY좌표를 확인한다
+// window.addEventListener('mousemove', (e) => {
+//     cursorEl.textContent = ""
+//     cursorEl.style.top =`${e.pageY}px`
+//     cursorEl.style.left = `${e.pageX}px`;
+//     cursorEl.textContent += `🎨 ${e.pageX}px, ${e.pageY}px`
+// })
+// //window에 mousemove 이벤트 추가
+// //실제적으로 mouse의 좌표를 확인하려면 mousemove
+// //mousemove 이벤트의 pageX, pageY좌표를 확인한다
+
